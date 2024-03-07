2a52d2f220295ad81ce3000af4f0581f97307120
msindunugroho <m.sindunugroho@gmail.com>
2024-03-08 00:42:18 +0700

    feat: add git basic concept module

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 import { anchorCreator } from "./components/anchorCreator.js";
 import { dropdownList } from "./components/dropdownList.js";
 import { navEventStyle } from "./components/navigation.js";
-import { ordered_list } from "./components/ordered_list.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 navEventStyle()
-ordered_list()
 anchorCreator()
 dropdownList()
 })
